aaa8032aa3ddabab5a2a5e81c6f17c9c3767aa6c
nukopy <pytwbf201830@gmail.com>
2021-12-11 20:48:14 +0900

    feat: add 回文判定

---
+++ tests/others/test_is_palindrome.py
@@ -0,0 +1,17 @@
+import pytest
+
+from library.others.is_palindrome import is_palindrome
+
+
+# parametrize
+@pytest.mark.parametrize(
+    "string,expected",
+    [
+        ("level", True),
+        ("algomethod", False),
+        ("xxxxxx", True),
+    ],
+)
+def test_is_palindrome(string, expected):
+    actual = is_palindrome(string)
+    assert actual == expected
